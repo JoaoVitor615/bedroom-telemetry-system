25b1295f411d86d5d3d1c9ed2660be73f7dfba8d
JoaoVitor615 <jvpeniza@gmail.com>
2026-08-12 23:32:55 -0300

    Add .gitignore to exclude config.go file and create config.example.go template

---
+++ firmware/config.example.go
@@ -0,0 +1,27 @@
+package main
+
+// #####################################################
+// Uncomment this file and rename it to config.go
+// #####################################################
+
+// // Wi-Fi Credentials
+// WifiSSID = "YOUR_WIFI_SSID"
+// WifiPass = "YOUR_WIFI_PASSWORD"
+
+// // MQTT Broker Credentials
+// MqttBroker   = "192.168.1.100"
+// MqttPort     = 1883
+// MqttClientID = "bedroom-esp32-node"
+
+// // MQTT Topics
+// TopicTemperature = "bedroom/temperature"
+// TopicDoorEvent   = "bedroom/door/event"
+
+// // Hardware Pins
+// PinSDA  = machine.GPIO21
+// PinSCL  = machine.GPIO22
+// PinDoor = machine.GPIO4
+
+// // Polling and telemetry intervals (in milliseconds)
+// TempTelemetryIntervalMs = 2000
+// DoorPollingIntervalMs   = 50
